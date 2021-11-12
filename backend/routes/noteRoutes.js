4a3c0997496d1ce3de893cd96a26fb00b35e999b
I322214 <sajal.gupta@sap.com>
2021-11-12 15:37:29 +0530

    Added a new api - delete a note and updated collection

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -119,4 +119,28 @@ router.put(
   }
 );
 
+
+// ROUTE 4: Delete a particular note of a particular logged in user based on the auth token : endpoint: /api/note/deleteNote
+router.delete(
+  "/deleteNote/:id",
+  fetchUser,
+  async (req, res) => {
+    try {
+      const noteById = await Notes.findById(req.params.id);
+      if (!noteById) {
+        return res.status(400).json("Note not found. Please check.");
+      }
+      // ensure that the logged in user is only deleting the note and no other user is doing
+      if (noteById.user.toString() !== req.user.id) {
+        return res.status(401).json("Incorrect user. Please check.");
+      }
+      const deletedNote = await Notes.findByIdAndDelete(req.params.id);
+      res.json(deletedNote);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json(`Oops. Error found: ${err.message}`);
+    }
+  }
+);
+
 module.exports = router;
